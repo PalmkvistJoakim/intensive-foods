ce95b8792f37437f35d3b0651a3c23b646253325
PalmkvistJoakim <jockepalmkvist@gmail.com>
2022-07-19 18:29:39 +0200

    added Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Foods from "./components/Foods";
+import Pagination from "./components/Pagination";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Foods />
+        <Pagination />
       </div>
     );
   }
